bbaa6d2e625307858a9e9521c60db632899604a3
jeromefiot <j_fiot@hotmail.com>
2015-12-07 12:48:37 +0100

    update

--- app/radio/views.py
+++ app/radio/views.py
@@ -140,7 +140,7 @@ def podcast(action):
         up_dir = current_app.config['UPLOAD_FOLDER']
         urlmusic = request.args.get('urlpodcast')
 
-        name_podcast = 'PODCAST_' + request.args.get('nompodcast').replace('"', '_')
+        name_podcast = 'PODCAST_' + request.args.get('nompodcast').replace(' ', '_')
 
         print "DIR : "+up_dir
         print "NOM : "+name_podcast
