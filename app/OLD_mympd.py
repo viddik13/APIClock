01730c72e6d5adf5cf58ea4e1bff6466da4d0933
doudettecorp <doudettecorp@gmail.com>
2015-12-19 15:01:41 +0100

    local play

---
+++ app/OLD_mympd.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+from mpd import MPDClient
+import time
+import argparse
+import podcastparser
+import urllib
+import pprint
+
+
+class player():
+
+    """Play arg "radio" in local HTML player."""
+
+    def __init__(self):
+        """Connection init."""
+        self.client = MPDClient()
+        self.client.timeout = 10
+        self.client.idletimeout = None
+        try:
+            self.client.connect("localhost", 6600)
+            self.client.update()
+        except Exception:
+            print "Can't Connect to MPD..."
+
+    def clear(self):
+        """Clear player playlist."""
+        self.client.clear()
+
+    def play(self,
+        media="http://audio.scdn.arkena.com/11010/franceculture-midfi128.mp3"):
+        """Start player with media(url) arg."""
+        self.client.clear()
+        self.client.add(media)
+        self.client.setvol(90)
+        self.client.play()
+
+    def podlist(self, media):
+        """Get podcast infos(parsed)."""
+        parsed = podcastparser.parse(media, urllib.urlopen(media))
+        return parsed
+
+    def stop(self):
+        """Stop player."""
+        self.client.stop()
+
+    def volup(self, n=5):
+        """Set player volume up."""
+        status = self.client.status()
+        nvol = int(status['volume']) + n
+        if nvol > 100:
+            nvol = 100
+        self.client.setvol(nvol)
+        print int(status['volume'])
+        print "Volume : %d" % nvol
+
+    def voldown(self, n=5):
+        """Set player volume down."""
+        status = self.client.status()
+        volume = status['volume']
+        nvol = int(volume) - n
+        if nvol < 0:
+            nvol = 0
+        self.client.setvol(nvol)
+        print int(status['volume'])
+        print "Volume : %d" % nvol
+
+    def status(self):
+        """Get player status."""
+        if (self.client.status()):
+            status = self.client.status()
+
+        monstatus = {}
+
+        for key, value in status.items():
+            monstatus[key] = value.encode('utf-8')
+
+        time.sleep(2)
+        maplaylist = self.client.playlistid()
+        try:
+            for key, value in maplaylist[0].items():
+                monstatus[key] = value.decode('utf-8')
+        except:
+            pass
+        return monstatus
+
+    def is_playing(self):
+        """Verify player playing and update globale MPDstatut."""
+        if self.status():
+            stat = self.status()
+            global MPDstatut
+            if stat['state'] != None:
+                MPDstatut = stat['state']
+            else:
+                MPDstatut = None
+        else:
+            MPDstatut = None
+        return MPDstatut
+
+
+def main():
+    """Playing media."""
+    parser = argparse.ArgumentParser(
+        description="This script play a given media on the mpd (local) server")
+    parser.add_argument("-c",
+        choices=['play', 'stop', 'status', 'volup', 'voldown', 'pod'],
+        required=True)
+    parser.add_argument("-u", help="url to play")
+
+    args = parser.parse_args()
+    media = args.u
+    command = args.c
+
+    myplayer = player()
+    if command == 'play':
+        if media is not None:
+            myplayer.play(media)
+        else:
+            print "You must enter a media url/path to play"
+    elif command == 'stop':
+        myplayer.stop()
+    elif command == 'volup':
+        myplayer.volup()
+    elif command == 'voldown':
+        myplayer.voldown()
+    elif command == 'status':
+        state = myplayer.status()
+        for cle, val in state.items():
+            print cle + " : " + val
+    elif command == 'podlist':
+        parsed = podcastparser.parse(media, urllib.urlopen(media))
+        pprint.pprint(parsed)
+
+
+if __name__ == "__main__":
+    main()
